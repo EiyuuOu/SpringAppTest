cd68b5873130b7ee360892c514d93cb7fb623788
EiyuuOu <bradas@gmx.de>
2017-01-26 23:16:39 +0100

    SpringApp annotations todo

---
+++ src/main/java/com/fdmgroup/repository/HibernateCustomerRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.fdmgroup.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fdmgroup.model.Customer;
+
+public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+	
+	/* (non-Javadoc)
+	 * @see com.fdmgroup.repository.CustomerRepository#findAll()
+	 */
+	@Override
+	public List<Customer> findAll(){
+		List<Customer> customers = new ArrayList<>();
+		
+		Customer customer = new Customer();
+		
+		customer.setFirstname("Niko");
+		customer.setLastname("Brah");
+		
+		customers.add(customer);
+		
+		return customers;
+	}
+
+}
